2b1144fa3d3cf05db9e8d1b7240929d2e7fd0de8
Colin <colin@shier.ca>
2026-02-19 19:39:18 -0500

    Added nav bar transitions

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const startButton = document.querySelector('.StartButton');
 const mainContent = document.getElementById('MainContent');
+const welcomeScreen = document.getElementById('WelcomeScreen');
+const homeBtn = document.getElementById('homeBtn');
 
 startButton.addEventListener('click',() => {
     mainContent.classList.add('splash-reveal');
@@ -11,4 +13,13 @@ startButton.addEventListener('click',() => {
         document.getElementById('WelcomeScreen').style.display = 'none';
     }, 1200)
 });
+
+homeBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+
+
+    mainContent.classList.remove('splash-reveal');
+    document.body.classList.remove('allow-scroll');
+    welcomeScreen.style.display = 'flex';
+})
 
