bc510caaffc2ebfed914ec4c69a1f7ce3d91183c
Colin <colin@shier.ca>
2026-02-20 13:58:56 -0500

    Particles/pfp

--- main.js
+++ main.js
@@ -17,10 +17,44 @@ startButton.addEventListener('click',() => {
 homeBtn.addEventListener('click',(e) => {
     e.preventDefault();
 
-
-
+    
+    
     mainContent.classList.remove('splash-reveal');
+
+    setTimeout(() => {
+        mainContent.scrolltop = 0;
+        window.scrollTo(0,0);
     document.body.classList.remove('allow-scroll');
     welcomeScreen.style.display = 'flex';
-})
+    },600);
+});
+
+particlesJS("particles-js",{
+    "particles":{
+        "number": {value:80,"density":{"enable":true,"value_area":800}},
+        "color":{"value":"#007bff"},
+        "shape":{"type":"circle"},
+        "opacity":{"value":0.5},
+        "size":{"value":3},
+        "line_linked":{
+            "enable":true,
+            "distance":150,
+            "color":"007bff",
+            "opacity":0.4,
+            "width":1
+
+        },
+
+        "move":{"enable":true,"speed":2}
+    },
+    
+    "interactivity":{
+        "events":{
+            "onhover":{"enable":true,"mode":"grab"},
+            "onclick":{"enable":true,"mode":"push"}
+        }
+    },
+
+    "retina_detect":true
+});
 
